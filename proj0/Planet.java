4ecce5ad1f574d63dc523e51d47e7ee0d0635bde
jimochance <102490057+jimochance@users.noreply.github.com>
2022-06-09 16:46:05 -0500

    Add files via upload

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -28,4 +28,80 @@ public class Planet {
 		double distance = this.calcDistance(p);
 		return (gravitionalConstant * this.mass	* p.mass) / (distance * distance);
 	}
+
+	public double calcForceExertedByX(Planet p) {
+		double netForce = this.calcForceExertedBy(p);
+		double xDistance =  p.xxPos - this.xxPos;
+		return (netForce * xDistance) / this.calcDistance(p);
+	}
+
+	public double calcForceExertedByY(Planet p) {
+		double netForce = this.calcForceExertedBy(p);
+		double yDistance = p.yyPos - this.yyPos;
+		return (netForce * yDistance) / this.calcDistance(p);
+	}
+
+	public double calcNetForceExertedByX(Planet[] allPlanets) {
+		double netForceX = 0.0;
+		for (int i = 0; i < allPlanets.length; i++) {
+			if (allPlanets[i].equals(this)) {
+				continue;
+			} else {
+				netForceX += this.calcForceExertedByX(allPlanets[i]);
+			}
+		}
+		return netForceX;
+	}
+
+	public double calcNetForceExertedByY(Planet[] allPlanets) {
+		double netForceY = 0.0;
+		for (int i = 0; i < allPlanets.length; i++) {
+			if (allPlanets[i].equals(this)) {
+				continue;
+			} else {
+				netForceY += this.calcForceExertedByY(allPlanets[i]);
+			}
+		}
+		return netForceY;
+	}
+
+	public void update(double dt, double fX, double fY) {
+		// 1. calculate the acceleration using the provided x and y forces
+		double acceX = fX / this.mass;
+		double acceY = fY / this.mass;
+
+		// 2. calculate the new velocity by using the acceleration and current velocity
+		this.xxVel = this.xxVel + dt * acceX;
+		this.yyVel = this.yyVel + dt * acceY;
+
+		// 3. calculate the new position by using the velocity computed in step 2 and the current
+	    //    position
+	    this.xxPos = this.xxPos + dt * this.xxVel;
+	    this.yyPos = this.yyPos + dt * this.yyVel;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
