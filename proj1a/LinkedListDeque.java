0ff2d1e137c30bda3f8e45abc5e5d6357694a3c6
jimochance <102490057+jimochance@users.noreply.github.com>
2022-06-21 19:18:07 -0500

    Add files via upload

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,31 +1,30 @@
 public class LinkedListDeque<T> {
     private class Node {
-        public Node pre;
-        public T item;
-        public Node next;
+        private Node prev;
+        private T item;
+        private Node next;
 
         public Node(Node p, T i, Node n) {
-            pre = p;
+            prev = p;
             item = i;
             next = n;
         }
     }
 
-    private Node sentFront;
-    private Node sentBack;
+    private Node sentinel;
     private int size;
 
     public LinkedListDeque() {
-        sentFront = new Node(null, null, null);
-        sentBack = new Node(null, null, null);
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
         size = 0;
     }
 
     public LinkedListDeque(T item) {
-        sentFront = new Node(null, null, null);
-        sentFront.next = new Node(null, item, null);
-        sentBack = new Node(null, null, null);
-        sentBack.pre = new Node(null, item, null);
+        sentinel = new Node(null, null, null);
+        sentinel.next = new Node(null, item, null);
+        sentinel.prev = sentinel.next;
         size = 1;
     }
 
@@ -41,31 +40,34 @@ public class LinkedListDeque<T> {
     }
 
     public void printDeque() {
-        Node prt = sentFront;
-        while (prt != null) {
-            System.out.print(prt.item + " ");
-            prt = prt.next;
+        Node node = sentinel;
+        while (node != sentinel) {
+            node = node.next;
+            System.out.print(sentinel.item + " ");
         }
         System.out.println();
     }
 
     public void addFirst(T item) {
+        sentinel.next = new Node(sentinel, item, sentinel.next);
+        sentinel.next.next.prev = sentinel.next;
         size += 1;
-        sentFront.next = new Node(sentBack.pre, item, sentFront.next);
     }
 
     public void addLast(T item) {
+        sentinel.prev = new Node(sentinel.prev, item, sentinel);
+        sentinel.prev.prev.next = sentinel.prev;
         size += 1;
-        sentBack.pre = new Node(sentBack.pre, item, sentFront.next);
     }
 
     public T removeFirst() {
         if (size == 0) {
             return null;
         }
+        T removed = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
         size -= 1;
-        T removed = sentFront.next.item;
-        sentFront.next = sentFront.next.next;
         return removed;
     }
 
@@ -73,9 +75,39 @@ public class LinkedListDeque<T> {
         if (size == 0) {
             return null;
         }
+        T removed = sentinel.prev.item;
+        sentinel.prev.prev = sentinel.prev;
+        sentinel.prev.next = sentinel;
         size -= 1;
-        T removed = sentBack.pre.item;
-        sentBack.pre = sentBack.pre.pre;
         return removed;
+    }
+
+    public T get(int index) {
+        int count = 0;
+        Node node = sentinel;
+        while (node.next != sentinel) {
+            node = node.next;
+            if (count == index) {
+                return node.item;
+            }
+            count++;
+        }
+        return null;
+    }
+
+    public T getRecursiveHelper(int index, int count, Node node) {
+        if (index == count) {
+            return node.item;
+        }
+        return getRecursiveHelper(index, count + 1, node.next);
+    }
+
+    public T getRecursive(int index) {
+        if (index >= size || index < 0) {
+            return null;
+        }
+        int count = 0;
+        Node node = sentinel.next;
+        return getRecursiveHelper(index, count, node);
     }
 }
